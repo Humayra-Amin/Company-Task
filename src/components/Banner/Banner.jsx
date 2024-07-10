9141f365e961f20684cfc3ccad671dcdc34c8d49
Humayra-Amin <humaema4576@gmail.com>
2024-07-10 13:12:18 +0600

    banner added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,62 +1,62 @@
-// import React from 'react';
-// import banner from '../../assets/images/Group 442.png';
-// import arrow from '../../assets/images/arrow_downward_alt.svg';
+import React from 'react';
+import banner from '../../assets/images/Group 442.png';
+import arrow from '../../assets/images/arrow_downward_alt.svg';
 
-// const Banner = () => {
-//     return (
-//         <div className="bg-[#2042B6] h-[650px] lg:h-[650px] flex items-center justify-center relative">
-//             <div className="absolute left-0 top-0 ml-24 mt-72 lg:ml-8 lg:mt-28 text-white text-center lg:text-start">
-//                 <div className="font-normal">
-//                     <h2 className='font1 text-2xl lg:text-3xl'>60</h2>
-//                     <p className='font2 text-[14px] lg:text-[16px]'>Companies</p>
-//                 </div>
-//                 <div className="font-normal mt-6 lg:mt-12">
-//                     <h2 className='font1 text-2xl lg:text-3xl'>$100B</h2>
-//                     <p className='font2 text-[14px] lg:text-[16px]'>Market cap</p>
-//                 </div>
-//                 <div className="font-normal mt-6 lg:mt-12">
-//                     <h2 className='font1 text-2xl lg:text-3xl'>3.5x</h2>
-//                     <p className='font2 text-[14px] lg:text-[16px]'>Revenue multiple</p>
-//                 </div>
-//                 <div className="font-normal mt-6 lg:mt-12">
-//                     <h2 className='font1 text-2xl lg:text-3xl'>60%</h2>
-//                     <p className='font2 text-[14px] lg:text-[16px]'>LTM avg. revenue growth rate</p>
-//                 </div>
-//             </div>
-//             <div className="relative">
-//                 <div
-//                     className="absolute inset-0 bg-center bg-cover mix-blend-multiply h-[300px] lg:h-[542px] w-[300px] lg:w-[549px] mt-[-339px] lg:mt-[-200px] ml-[20px] lg:ml-[40px]"
-//                     style={{ backgroundImage: `url(${banner})`,  opacity: '0.9'}}
-//                 />
-//                 <div className="relative text-center text-white z-10 mt-[-14.5rem] lg:mt-0">
-//                     <h1 className="text-2xl lg:text-6xl font-normal font1">The WPPOOL Index</h1>
-//                     <p className="py-2 lg:py-6 font text-sm lg:text-base">Monitor the performance of emerging, publicly traded, financial technology companies</p>
-//                 </div>
-//                 <div className="absolute top-[-12rem] lg:top-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0">
-//                     <svg xmlns="http://www.w3.org/2000/svg" width="290" height="257" viewBox="0 0 579 515" fill="none" className="lg:w-[579px] lg:h-[515px]">
-//                         <g filter="url(#filter0_d_1_759)">
-//                             <path d="M44.9268 470.5L324.841 44.5H534.073L254.159 470.5H44.9268Z" stroke="white" shape-rendering="crispEdges" />
-//                         </g>
-//                         <defs>
-//                             <filter id="filter0_d_1_759" x="0" y="0" width="579" height="515" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-//                                 <feFlood flood-opacity="0" result="BackgroundImageFix" />
-//                                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-//                                 <feOffset />
-//                                 <feGaussianBlur stdDeviation="22" />
-//                                 <feComposite in2="hardAlpha" operator="out" />
-//                                 <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.66 0" />
-//                                 <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_759" />
-//                                 <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_759" result="shape" />
-//                             </filter>
-//                         </defs>
-//                     </svg>
-//                 </div>
-//             </div>
-//             <div className="absolute bottom-4 right-4 text-white">
-//                <img src={arrow} className='border bg-white rounded-full shadow-2xl w-6 h-6 lg:w-10 lg:h-10' />
-//             </div>
-//         </div>
-//     );
-// };
+const Banner = () => {
+    return (
+        <div className="bg-[#2042B6] h-[650px] lg:h-[650px] flex items-center justify-center relative">
+            <div className="absolute left-0 top-0 ml-24 mt-72 lg:ml-8 lg:mt-28 text-white text-center lg:text-start">
+                <div className="font-normal">
+                    <h2 className='font1 text-2xl lg:text-3xl'>60</h2>
+                    <p className='font2 text-[14px] lg:text-[16px]'>Companies</p>
+                </div>
+                <div className="font-normal mt-6 lg:mt-12">
+                    <h2 className='font1 text-2xl lg:text-3xl'>$100B</h2>
+                    <p className='font2 text-[14px] lg:text-[16px]'>Market cap</p>
+                </div>
+                <div className="font-normal mt-6 lg:mt-12">
+                    <h2 className='font1 text-2xl lg:text-3xl'>3.5x</h2>
+                    <p className='font2 text-[14px] lg:text-[16px]'>Revenue multiple</p>
+                </div>
+                <div className="font-normal mt-6 lg:mt-12">
+                    <h2 className='font1 text-2xl lg:text-3xl'>60%</h2>
+                    <p className='font2 text-[14px] lg:text-[16px]'>LTM avg. revenue growth rate</p>
+                </div>
+            </div>
+            <div className="relative">
+                <div
+                    className="absolute inset-0 bg-center bg-cover mix-blend-multiply h-[300px] lg:h-[542px] w-[300px] lg:w-[549px] mt-[-339px] lg:mt-[-200px] ml-[20px] lg:ml-[40px]"
+                    style={{ backgroundImage: `url(${banner})`,  opacity: '0.9'}}
+                />
+                <div className="relative text-center text-white z-10 mt-[-14.5rem] lg:mt-0">
+                    <h1 className="text-2xl lg:text-6xl font-normal font1">The WPPOOL Index</h1>
+                    <p className="py-2 lg:py-6 font text-sm lg:text-base">Monitor the performance of emerging, publicly traded, financial technology companies</p>
+                </div>
+                <div className="absolute top-[-12rem] lg:top-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="290" height="257" viewBox="0 0 579 515" fill="none" className="lg:w-[579px] lg:h-[515px]">
+                        <g filter="url(#filter0_d_1_759)">
+                            <path d="M44.9268 470.5L324.841 44.5H534.073L254.159 470.5H44.9268Z" stroke="white" shape-rendering="crispEdges" />
+                        </g>
+                        <defs>
+                            <filter id="filter0_d_1_759" x="0" y="0" width="579" height="515" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                <feOffset />
+                                <feGaussianBlur stdDeviation="22" />
+                                <feComposite in2="hardAlpha" operator="out" />
+                                <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.66 0" />
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_759" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_759" result="shape" />
+                            </filter>
+                        </defs>
+                    </svg>
+                </div>
+            </div>
+            <div className="absolute bottom-4 right-4 text-white">
+               <img src={arrow} className='border bg-white rounded-full shadow-2xl w-6 h-6 lg:w-10 lg:h-10' />
+            </div>
+        </div>
+    );
+};
 
-// export default Banner;
+export default Banner;
